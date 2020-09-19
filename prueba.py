9cd4b251783c55079e05880014d0ecdb31c8eb1d
Ramiro Uribe Garrido <ramiro.uribe.garrido@gmail.com>
2020-09-19 19:11:40 -0300

    Version 4

--- prueba.py
+++ prueba.py
@@ -4,13 +4,26 @@ y=9
 i=0
 suma=x+y
 print(suma)
-lista=[]
-for i in range(10):
-    lista.append(i)
+lista2=[]
+lista1=[]
+lista3=[]
+for i in range(4):
+    lista1.append(i)
 
-for i in range(10):
-    print(lista[i])
-lista.sort()
+for i in lista1:
+    print(lista1[i])
+for i in lista1:
+    x=int(input("Ingrese numero para la lista: "))
+    lista2.append(x)
 
 
-print(lista)
+lista1.sort()
+lista2.sort()
+lista3=[lista1,lista2]
+
+
+print(lista1)
+print(lista2)
+print(lista3)
+type(lista3)
+
