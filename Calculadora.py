53ba0085f4e63d8245da14719243e53cd1883f5a
Ramiro Uribe Garrido <ramiro.uribe.garrido@gmail.com>
2020-11-01 15:11:10 -0300

    Calculadora validada

--- Calculadora.py
+++ Calculadora.py
@@ -15,39 +15,52 @@ def calculadora():
     valor=0
     valor1=0
     total=0
-    valor=float(input("Ingrese número 1: "))
-    while(opc==0):
-        
-        opc=input("Ingrese operación ")
-        valor1=float(input("Ingrese número 2: "))
-        ter=str(input("Ingrese = si desea terminar o 0 si desea continuar: "))
-        if(opc=='+'):
-            if(ter=='='):
-                print (suma(valor,valor1))
-                opc=1
-            else:
-                valor = suma(valor,valor1)
-                opc=0
-        if(opc=='-'):
-            if(ter=='='):
-                print(resta(valor,valor1))
-                opc=1
-            else:
-                valor=resta(valor,valor1)
-                opc=0
-        if(opc=='*'):
-            if(ter=='='):
-                print(producto(valor,valor1))
-                opc=1
-            else:
-                valor=producto(valor,valor1)
-                opc=0
-        if(opc=='/'):
-            if(ter=='='):
-                print(division(valor1,valor))
-                opc=1
-            else:
-                valor=division(valor1,valor)
-                opc=0
-
+    valor=(input("Ingrese número 1: "))
+    try:
+        valor=float(valor)
+    except:
+        valor='No'
+    if(valor=='No'):
+        exit()
+    if (valor=='No'):
+        print("Ingresaste mal el valor, intenta nuevamente")
+    else:
+        while(opc==0):
+            opc=input("Ingrese operación ")
+            valor1=(input("Ingrese número 2: "))
+            try:
+                valor1=float(valor1)
+            except:
+                valor1='No'
+            if(valor1=='No'):
+                exit()
+            ter=(input("Ingrese = si desea terminar o 0 si desea continuar: "))
+            if(opc=='+'):
+                if(ter=='='):
+                    print (suma(valor,valor1))
+                    opc=1
+                else:
+                    valor = suma(valor,valor1)
+                    opc=0
+            if(opc=='-'):
+                if(ter=='='):
+                    print(resta(valor,valor1))
+                    opc=1
+                else:
+                    valor=resta(valor,valor1)
+                    opc=0
+            if(opc=='*'):
+                if(ter=='='):
+                    print(producto(valor,valor1))
+                    opc=1
+                else:
+                    valor=producto(valor,valor1)
+                    opc=0
+            if(opc=='/'):
+                if(ter=='='):
+                    print(division(valor1,valor))
+                    opc=1
+                else:
+                    valor=division(valor1,valor)
+                    opc=0
 calculadora()
